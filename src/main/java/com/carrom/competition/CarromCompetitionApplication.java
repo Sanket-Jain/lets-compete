96d82e2ecbd67ae660255aef24a4cd3e44a3277b
sanketjain <39269430+Sanket-Jain@users.noreply.github.com>
2026-05-20 17:22:22 +0530

    Fixed Tab links.

--- src/main/java/com/carrom/competition/CarromCompetitionApplication.java
+++ src/main/java/com/carrom/competition/CarromCompetitionApplication.java
@@ -2,9 +2,17 @@ package com.carrom.competition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarromCompetitionApplication {
+public class CarromCompetitionApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CarromCompetitionApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CarromCompetitionApplication.class, args);
     }
---
+++ src/main/java/com/carrom/competition/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.carrom.competition.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(1);
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/WEB-INF/static/");
+    }
+}
